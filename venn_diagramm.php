4aebf5556de04570874cf30ee6fb055d731d934b
TravisCG <buldozer88@hotmail.com>
2018-10-11 11:36:55 +0200

    Removing unecessary codes: part 2

--- venn_diagramm.php
+++ venn_diagramm.php
@@ -860,8 +860,6 @@ foreach($jsonData4 as $item){
 <br>
 <br>
 
-
-
 <script>
 
 //this thing will help the preselect trim the experiment selection
@@ -927,9 +925,6 @@ $('select#cellformexp1v2').click(function(){
 $("select#formexp1v2 option").removeClass("cell_unselected");
 $("select#formexp1v2 " + "option:not([data-celline='" + $("#cellformexp1v2").val() + "'])" ).addClass("cell_unselected");
 
-//$("select#antiformexp1v2 option").attr('disabled', 'disabled');
-//$(  "select#antiformexp1v2 " +  "option[data-celline~=\"" +  $("#cellformexp1v2").val() + "\"]" )
-//        .attr( 'disabled', false )
 
   });
 }); 
@@ -940,10 +935,6 @@ $('select#cellformexp2v2').change(function(){
 $("select#formexp2v2 option").removeClass("cell_unselected");
 $("select#formexp2v2 " + "option:not([data-celline='" + $("#cellformexp2v2").val() + "'])" ).addClass("cell_unselected");
 
-//$("select#antiformexp2v2 option").attr('disabled', 'disabled');
-//$(  "select#antiformexp2v2 " +  "option[data-celline~=\"" +  $("#cellformexp2v2").val() + "\"]" )
-//        .attr( 'disabled', false )
-
   });
 }); 
 
@@ -952,11 +943,6 @@ $('select#cellformexp3v2').change(function(){
 
 $("select#formexp3v2 option").removeClass("cell_unselected");
 $("select#formexp3v2 " + "option:not([data-celline='" + $("#cellformexp3v2").val() + "'])" ).addClass("cell_unselected");
-
-//$("select#antiformexp3v2 option").attr('disabled', 'disabled');
-//$(  "select#antiformexp3v2 " +  "option[data-celline~=\"" +  $("#cellformexp3v2").val() + "\"]" )
-//        .attr( 'disabled', false )
-
   });
 }); 
 //make the buttons work
@@ -993,20 +979,13 @@ $('button#exp3v2').click(function(){
 
 
 </div>
-
-
 <br>
-
-
-
 <p>After setting the parameters, click the button below to refresh the page.</p>
 
 <button id="resend2" onclick="doSearchVenn2()" style="width: 14em;"><p>Refresh Page</p></button>
 
 </div>
 </div>
-
-
 
 <script>
 
@@ -1049,18 +1028,13 @@ var formexp1value = <?php echo  $exp1Name ; ?>;
 document.getElementById("formexp1").value = formexp1value;
 document.getElementById("formexp1v2").value = formexp1value;
 
-
 var formexp2value = <?php echo  $exp2Name ; ?>;
 document.getElementById("formexp2").value = formexp2value;
 document.getElementById("formexp2v2").value = formexp2value;
 
-
 var formexp3value = <?php echo  $exp3Name ; ?>;
 document.getElementById("formexp3").value = formexp3value;
 document.getElementById("formexp3v2").value = formexp3value;
-
-
-
 
 var formmotive = <?php echo '"'. $motifPart . '"'; ?>;
 document.getElementById("formmotive").value = formmotive;
@@ -1078,42 +1052,17 @@ var lolz2 = data2start[0].cellid;
 document.getElementById("cellformexp2").value = lolz2;
 var lolz3 = data3start[0].cellid;
 document.getElementById("cellformexp3").value = lolz3;
-
-
 document.getElementById("antiformexp1v2").value = lol1;
-
 document.getElementById("antiformexp2v2").value = lol2;
-
 document.getElementById("antiformexp3v2").value = lol3;
-
-
 document.getElementById("cellformexp1v2").value = lolz1;
-
 document.getElementById("cellformexp2v2").value = lolz2;
-
 document.getElementById("cellformexp3v2").value = lolz3;
 
 
 var formminelem = getAllUrlParams().mnelem;
 document.getElementById("textboxmnelem").value = formminelem;
 
-
-
-//arata_formularcell(formexp1,fexp1cell);
-//arata_formularcell(formexp2,fexp2cell);
-//arata_formularcell(formexp3,fexp3cell);
-//arata_formularanti(formexp1,fexp1anti);
-//arata_formularanti(formexp2,fexp2anti);
-//arata_formularanti(formexp3,fexp3anti);
-
-//arata_formularcell2(antiformexp1,fexp1cell);
-//arata_formularcell2(antiformexp2,fexp2cell);
-//arata_formularcell2(antiformexp3,fexp3cell);
-
-</script>
-
-
-<script>
 //this will generate the venn diagram for us
 
 document.getElementById("data").innerHTML = data - intersect1_2 - intersect1_3 + intersect1_2_3; 
@@ -1124,116 +1073,10 @@ document.getElementById("data1i3").innerHTML = intersect1_3 - intersect1_2_3;
 document.getElementById("data2i3").innerHTML = intersect2_3 - intersect1_2_3; 
 document.getElementById("data1i2i3").innerHTML = intersect1_2_3; 
 
-
 </script>
-
-
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore.js">
 </script>
-
-<script>
-//this script here will generate us the intersections for the venn diagramm bed file downloads
-
-
-   function intersectionObjects2(a, b, areEqualFunction) {
-        var results = [];
-        
-        for(var i = 0; i < a.length; i++) {
-            var aElement = a[i];
-            var existsInB = _.any(b, function(bElement) { return areEqualFunction(bElement, aElement); });
-
-            if(existsInB) {
-                results.push(aElement);
-            }
-        }
-        
-        return results;
-    }
-
-
-
-function intersectionObjects() {
-        var results = arguments[0];
-        var lastArgument = arguments[arguments.length - 1];
-        var arrayCount = arguments.length;
-        var areEqualFunction = _.isEqual;
-        
-        if(typeof lastArgument === "function") {
-            areEqualFunction = lastArgument;
-            arrayCount--;
-        }
-        
-        for(var i = 1; i < arrayCount ; i++) {
-            var array = arguments[i];
-            results = intersectionObjects2(results, array, areEqualFunction);
-            if(results.length === 0) break;
-        }
-
-        return results;
-    }
-
-
-   function differenceObjects2(a, b, areEqualFunction) {
-        var results = [];
-        
-        for(var i = 0; i < a.length; i++) {
-            var aElement = a[i];
-            var existsInB = _.any(b, function(bElement) { return areEqualFunction(bElement, aElement); });
-
-            if(!existsInB) {
-		var pololo = "#11111";
-		var aElementfinal = aElement.concat(pololo);
-                results.push(aElementfinal);
-            }
-        }
-        
-        return results;
-    }
-
-
-
-function differenceObjects() {
-        var results = arguments[0];
-        var lastArgument = arguments[arguments.length - 1];
-        var arrayCount = arguments.length;
-        var areEqualFunction = _.isEqual;
-
-        if(typeof lastArgument === "function") {
-            areEqualFunction = lastArgument;
-            arrayCount--;
-        }
-
-        for(var i = 1; i < arrayCount ; i++) {
-            var array = arguments[i];
-            results = differenceObjects2(results, array, areEqualFunction);
-            if(results.length === 0) break;
-        }
-
-        return results;
-    }
-
-
-
-
-    var resulti1v2v3 = intersectionObjects(data1list, data2list, data3list, function(item1, item2) {
-        return item1.motifpos_id === item2.motifpos_id;
-    });
-
-    var result1v2 = intersectionObjects(data1list, data2list,  function(item1i, item2i) {
-        return item1i.motifpos_id === item2i.motifpos_id;
-    });
-
-   var result1v2i = differenceObjects(result1v2, resulti1v2v3,  function(item1e, item2e) {
-        return item1e.motifpos_id === item2e.motifpos_id;
-    });
-
-var result1i = differenceObjects(data1list, data2list, data3list, function(item1e, item2e) {
-        return item1e.motifpos_id === item2e.motifpos_id;
-    });
-
-</script>
-
 
 <script src="urlgetter.js">//this one gets the options out of the url and make them an object
 </script>
@@ -1244,9 +1087,6 @@ var result1i = differenceObjects(data1list, data2list, data3list, function(item1
 <script src="buttons.js">//this will make the buttons work
 </script>
 
-
-
-
 <div style="width:100%">
 
 <p>
@@ -1254,11 +1094,6 @@ VennView<br><br>
 In this mode, two or three experiments can be compared. The values in the sections of the diagram indicates the number of common and specific peaks at a consensus motif binding site. In this view, the con$
 </p>
 <br>
-<br>
-<br>
-
-
-
 <p>
 Copyright © 2018    Mátyás Schiller,  Erik Czipa, Levente Kontra, Tibor Nagy, Júlia Koller,
 Orsolya Pálné Szén, Csaba Papp, László Steiner, Ferenc Marincs and Endre Barta
@@ -1266,5 +1101,4 @@ Orsolya Pálné Szén, Csaba Papp, László Steiner, Ferenc Marincs and Endre Ba
 </div>
 
 </body>
-
 </html>
