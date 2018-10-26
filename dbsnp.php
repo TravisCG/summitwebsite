24b7e6d22ed92243ac961ac3372dc4496f7820a3
TravisCG <buldozer88@hotmail.com>
2018-10-26 12:21:35 +0200

    Motif drawing improved

--- dbsnp.php
+++ dbsnp.php
@@ -10,37 +10,80 @@
   }
 
   function motifsbydbsnp($conn, $dbsnpid){
-    $sql = "select * from motif_pos left join dbsnp on (dbsnp.chr = motif_pos.chr and dbsnp.end > motif_pos.start and dbsnp.start < motif_pos.end) where dbsnp.dbsnp_id = '$dbsnpid';";
-    $result = sql2array($conn, $sql);
+    $sql    = "select * from motif_pos where dbsnp_id = '$dbsnpid';";
+    //$snps   = sql2array($conn, $sql);
+    $start  = $snps[2] - 50; //SNPs just one nucleotide long, we need a bigger picture.
+    $end    = $snps[3] + 50;
+    $sql    = "select * from motif_pos left join consensus_motif on (motif_pos.consensus_motif_motif_id = consensus_motif.motif_id) where motif_pos.chr = '"
+               . $snps[1] . "' and motif_pos.start > $start and motif_pos.end < $end;"; //TODO what about partial overlap?
+    $motifs = sql2array($conn, $sql);
+
+    $result           = [];
+    $result["motifs"] = $motifs;
+    $result["snps"]   = $snps;
+    $result["start"]  = $start;
+    $result["end"]    = $end;
     return($result);
   }
 
   function motifsbyregion($conn, $chr, $start, $end){
-    $sql = "";
-    $result = sql2array($conn, $sql);
+    $sql    = "select * from motif_pos left join consensus_motif on (consensus_motif_motif_id = motif_id) where chr = '$chr' and start > $start and end < $end order by start;";
+    $motifs = sql2array($conn, $sql);
+    $sql    = "select * from dbsnp where chr = '$chr' and start > $start and end < $end";
+    //$snps   = sql2array($conn, $sql);
+
+    $result           = [];
+    $result["motifs"] = $motifs;
+    $result["snps"]   = $snps;
+    $result["start"]  = $start;
+    $result["end"]    = $end;
     return($result);
   }
 
-  function drawMotif($motif, $width, $halfh){
-    $third = $halfh / 3;
+  function drawSNP($snp, $width, $halfh, $gstart, $gend){
+    $third  = $halfh / 3;
     $vstart = $halfh + $third;
-    $vend = $vstart + $third / 2;
-    $halfw = $width / 2;
+    $vend   = $vstart + $third / 2;
+    $pos    = ($snp[2] - $gstart) / ($gend - $gstart) * $width;
 
-    echo('<line x1="'.$halfw.'" y1="' . $vstart . '" x2="'.$halfw.'" y2="' . $vend . '" style="stroke:red;stroke-width=4;" />');
-    echo('<text x="'. $halfw .'" y="' . ($vend + 10) . '" text-anchor="middle" style="font:13px sans-serif;">' . $motif[8] . '</text>');
-
-    echo('<rect />');
+    //TODO If there are too many SNPs, the labels will collide
+    echo('<line x1="' . $pos . '" y1="' . $vstart . '" x2="' . $pos . '" y2="' . $vend . '" style="stroke:red;stroke-width=4;" />');
+    echo('<text x="' . $pos . '" y="' . ($vend + 10) . '" text-anchor="middle" style="font:13px sans-serif;">' . $snp[0] . '</text>');
   }
 
-  function drawAllMotifs($motifs, $width, $height){
-    $halfh = $height / 2;
+  function drawMotifs($motif, $width, $halfh, $gstart, $gend, $boxh){
+    $pos  = ($motif[2] - $gstart) / ($gend - $gstart) * $width;
+    $w    = ($motif[3] - $motif[2]) / ($gend - $gstart) * $width;
+    $texth = 13;
+    $textbline = ($boxh - $texth) / 2;
+    echo('<rect x="' . $pos . '" y="' . ($halfh - $boxh) . '" width="' . $w . '" height="' . $boxh . '" style="fill:pink;stroke:green;"/>');
+    echo('<text x="' . $pos . '" y="' . ($halfh - $textbline) . '" style="font:' . $texth . 'px sans-serif;">' . $motif[9] . '</text>');
+  }
+
+  function drawAllMotifs($feats, $width, $height){
+    $halfh     = $height / 2;
+    $gstart    = $feats["start"];
+    $gend      = $feats["end"];
+    $motifboxh = 16; // height of motif box
+    $boxsep    = 4;  // spacer between motif boxes
+
     echo('<svg width="' . $width . '" height="' . $height . '">');
     echo('<rect x="0" y="0" width="'.$width.'" height="'.$height.'" style="fill:lightblue;storoke:red;" />');
     echo('<line x1="0" y1="'.$halfh.'" x2="'.$width.'" y2="'.$halfh.'" style="stroke:black;stroke-width=2" />');
     
-    for($i = 0; $i < sizeof($motifs); $i++){
-      drawMotif($motifs[$i], $width, $halfh);
+    for($i = 0; $i < sizeof($feats["snps"]); $i++){
+      drawSNP($feats["snps"][$i], $width, $halfh, $gstart, $gend);
+    }
+
+    $base = $halfh;
+    for($i = 0; $i < sizeof($feats["motifs"]); $i++){
+      if(($i > 0) && ($feats["motifs"][$i][2] < $feats["motifs"][$i-1][3])){
+        $base = $base - $motifboxh - $boxsep;
+      }
+      else{
+        $base = $halfh;
+      }
+      drawMotifs($feats["motifs"][$i], $width, $base, $gstart, $gend, $motifboxh);
     }
     echo("</svg>");
   }
@@ -94,7 +137,7 @@ End position:<input id="inpend" type="text" name="end" value="<?php echo $end ?>
 <div>
 <?php
   if(isset($motifs)){
-    drawAllMotifs($motifs, 400, 300);
+    drawAllMotifs($motifs, 700, 300);
   }
 ?>
 </div>
